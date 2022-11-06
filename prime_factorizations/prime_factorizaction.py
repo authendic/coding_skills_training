f1656ed8d6781e876a95b373d914d435190d9536
daniel <danielliwd86@gmaile.com>
2022-11-07 00:53:48 +0800

    add all_factors list

--- prime_factorizations/prime_factorizaction.py
+++ prime_factorizations/prime_factorizaction.py
@@ -10,79 +10,44 @@
 ###############################################################################
 
 # >> 1
-import math
 
-def prime_candidates(from_include, to_include):
-    if from_include <= 3:
-        yield 2
-        yield 3
-        from_include = 5
-
-    if from_include % 6 <=1:
-        beg = from_include - from_include % 6 + 1
-        if beg < to_include:
-            yield beg
-        else:
-            return
-    else:
-        beg = from_include - from_include % 6 + 5
-
-    lastend = 0
+def prime_candidates(to_include):
+    yield 2
+    yield 3
+    beg = 5
     if to_include % 6 == 0:
-        lastend = to_include - 1
-        # adjust end to 6n+1
-        end = lastend - 4
-    elif to_include % 6 == 5:
-        lastend = to_include
-        # adjust end to 6n+1
-        end = lastend - 4
-    else:
-        # adjust end to 6n+1
-        end = to_include - to_include % 6 + 1
+        to_include -= 1
+    end = to_include - to_include % 6 - 1
     while beg <= end:
         yield beg
         yield beg + 2
         beg += 6
-    if lastend:
-        yield lastend
-
-
-def prime_candidates2(from_include, to_include):
-    # prime_candidates designed for large then 7,
-    # so if from_include less then 7,
-    # the function will allways yield 2 3 5.
-    if from_include < 7:
-        yield 2
-        yield 3
-        yield 5
-        from_include = 7
-
-    if from_include % 6 <= 1:
-        beg = from_include - from_include % 6 + 1
-    else:
-        beg = from_include - from_include % 6 + 5
-        if beg < to_include:
-            yield beg
-        else:
-            return
-        beg += 2
-    lastend = 0
-    if to_include % 6 == 0:
-        end = to_include - 1
-    elif to_include % 6 <= 4:
-        lastend = to_include - to_include % 6 + 1
-        end = lastend - 2
-    else:
-        end = to_include
-    while beg <= end:
+    if beg == to_include:
         yield beg
-        yield beg + 4
-        beg += 6
-    if lastend:
-        yield lastend
 
 
-def prime_factors(n):
+def prime_factors_v2(n):
+    if n < 2:
+        return []
+    factors = [0]
+    f_counts = [0]
+    for p in prime_candidates(n**0.5):
+        while n % p == 0:
+            if p != factors[-1]:
+                factors.append(p)
+                f_counts.append(1)
+            else:
+                f_counts[-1] += 1
+            n = n // p
+            if n == 1:
+                break
+    if n != 1:
+        factors.append(n)
+        f_counts.append(1)
+    return factors[1:], f_counts[1:]
+
+
+def prime_factors_v1(n):
     if n == 0:
         return []
     if n < 3:
@@ -92,7 +57,7 @@ def prime_factors(n):
         end = n // 2
     else:
         end = int(n ** 0.5)
-    for p in prime_candidates(2, end):
+    for p in prime_candidates(end):
         while n % p == 0:
             factors.append(p)
             n = n // p
@@ -101,6 +66,29 @@ def prime_factors(n):
     if n != 1:
         factors.append(n)
     return factors
+
+
+def choice_combine(*pows):
+    if len(pows) == 1:
+        for n in range(pows[0]+1):
+            yield [n]
+    if len(pows) > 1:
+        for n in range(pows[0]+1):
+            for pl in choice_combine(*pows[1:]):
+                yield [n] + pl
+    return []
+
+
+def all_factors(n):
+    facts, fcnts = prime_factors_v2(n)
+    factors = []
+    for c in choice_combine(*fcnts):
+        v = 1
+        for i in range(len(c)):
+            v *= facts[i] ** c[i]
+        factors.append(v)
+    return factors
+
 
 def isprime(n):
     if n < 2:
@@ -111,30 +99,24 @@ def isprime(n):
         end = n // 2
     else:
         end = int(n ** 0.5)
-    for p in prime_candidates(2, end):
+    for p in prime_candidates(end):
         if n % p == 0:
             return n == p
     return True
 
+
 # >> 1
 
 if __name__ == "__main__":
-    print(prime_factors(20))
-    print(prime_factors(228))
-    print(prime_factors(1599998640000299))
-    print(prime_factors(1599998640000289))
-    print(prime_factors(10000330202725801))
-    print(isprime(1599998640000289))
+    print(prime_factors_v2(20))
+    print(prime_factors_v2(228))
+    print(prime_factors_v2(1599998640000299))
+    print(prime_factors_v2(1599998640000289))
+    print(prime_factors_v2(10000330202725801))
+    print(isprime(1599998640000289))  # false
     print(isprime(39999983))
     print(isprime(100001651))
-    for c in prime_candidates(10000330202725801, 10000330202725801+100):
-        if isprime(c):
-            print(c)
-    print(list(prime_candidates(100, 200)))
-
-#     import sympy
-#     %timeit -n 10 sympy.isprime(10000330202725901)
-#     %timeit -n 3 isprime(10000330202725901)
-#     %timeit -n 10 pow(2, 10000330202725901, 10000330202725901)
+    print(all_factors(1599998640000289))
+    print(sorted(all_factors(1599998640000299)))
 
 # >> end
